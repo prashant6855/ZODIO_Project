eced4fe57f21fb9eef4f8f340093fd0193289a6a
abc <abc@LAPTOP-C33M8RAU>
2025-05-05 17:04:50 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Zodio_project {
+}
---
+++ src/project_1/project_1.java
@@ -0,0 +1,11 @@
+package project_1;
+
+public class project_1 {
+
+	public static void main(String[] args) {
+		System.out.println("prashant is a good BOY");
+		// TODO Auto-generated method stub
+
+	}
+
+}
