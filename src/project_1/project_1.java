e410860eab9064ace208db5e697de60d7e29b0a5
abc <abc@LAPTOP-C33M8RAU>
2025-05-05 17:18:49 +0530

    java

--- src/project_1/project_1.java
+++ src/project_1/project_1.java
@@ -4,7 +4,7 @@ public class project_1 {
 
 	public static void main(String[] args) {
 		System.out.println("prashant is a good BOY");
-		// TODO Auto-generated method stub
+		System.out.println("Hello Prashant");
 
 	}
 
