34a1f10efff2762139bbb2699049723d4dcf2f6a
abc <abc@LAPTOP-C33M8RAU>
2025-05-05 17:25:58 +0530

    push

--- src/project_1/project_1.java
+++ src/project_1/project_1.java
@@ -3,8 +3,9 @@ package project_1;
 public class project_1 {
 
 	public static void main(String[] args) {
-		System.out.println("prashant is a good BOY");
 		System.out.println("Hello Prashant");
+		System.out.println("prashant is a good BOY");
+		
 
 	}
 
